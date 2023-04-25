287b8d2f73e8b03c9b36dff97be5f1bfeb8be737
Evgeniya Astryakova <126478177+Evgeniya689@users.noreply.github.com>
2023-04-25 19:00:50 +0300

    Delete src directory

--- src/astryakova/company/Figure.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author evgen
- * Class Figure for getting information about square and perimeter for different figures
- */
-package astryakova.company;
-public interface Figure {
-    double getSquare();
-    double getPerimeter();
-}
